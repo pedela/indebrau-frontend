34f245f84bd0395df2291bb2d03b056c4b5fbce8
Peter de Lange <lange@dbis.rwth-aachen.de>
2019-03-19 00:53:56 +0100

    small adjustments

--- components/GraphChart.js
+++ components/GraphChart.js
@@ -4,6 +4,7 @@ import LineChart from 'recharts/lib/chart/LineChart';
 import Line from 'recharts/lib/cartesian/Line';
 import XAxis from 'recharts/lib/cartesian/XAxis';
 import YAxis from 'recharts/lib/cartesian/YAxis';
+import Typography from '@material-ui/core/Typography';
 import CartesianGrid from 'recharts/lib/cartesian/CartesianGrid';
 import Tooltip from 'recharts/lib/component/Tooltip';
 import Legend from 'recharts/lib/component/Legend';
@@ -14,7 +15,7 @@ import Error from './Error';
 const GRAPH_QUERY = gql`
   query GRAPH_QUERY($id: ID!) {
     graph(id: $id) {
-      graphData(last: 100, orderBy: time_ASC) {
+      graphData(last: 1000, orderBy: time_ASC) {
         time
         value
       }
@@ -27,8 +28,12 @@ function GraphChart() {
   const tempId = { id: 'cjtal4hjv005m0808e10lbyr2' };
   const idTwo = { id: 'cjteu8hdt01wk08196nvoix9c' };
   const idThree = { id: 'cjtevr4cn037808192d3kp6l2' };
+
   return (
     <div>
+      <Typography variant="h4" gutterBottom component="h2">
+        Temperature Fridge
+      </Typography>
       <Query query={GRAPH_QUERY} variables={tempId}>
         {({ data, error, loading }) => {
           if (loading) return <p>Loading...</p>;
@@ -37,7 +42,7 @@ function GraphChart() {
             // success! Prep timestamps...
             data = data.graph.graphData;
             for (var i = 0; i < data.length; i++) {
-              data[i].time = new Date(data[i].time).toLocaleString('en-GB');
+              // data[i].time = new Date(data[i].time).toLocaleString('en-GB');
             }
           }
           return (
@@ -61,6 +66,9 @@ function GraphChart() {
           );
         }}
       </Query>
+      <Typography variant="h4" gutterBottom component="h2">
+        Tilt iSpindel 2
+      </Typography>
       <Query query={GRAPH_QUERY} variables={idTwo}>
         {({ data, error, loading }) => {
           if (loading) return <p>Loading...</p>;
@@ -69,7 +77,7 @@ function GraphChart() {
             // success! Prep timestamps...
             data = data.graph.graphData;
             for (var i = 0; i < data.length; i++) {
-              data[i].time = new Date(data[i].time).toLocaleString('en-GB');
+              // data[i].time = new Date(data[i].time).toLocaleString('en-GB');
             }
           }
           return (
@@ -86,13 +94,16 @@ function GraphChart() {
                   dataKey="value"
                   stroke="#8884d8"
                   activeDot={{ r: 8 }}
-                  name="Temperature"
+                  name="Tilt"
                 />
               </LineChart>
             </ResponsiveContainer>
           );
         }}
       </Query>
+      <Typography variant="h4" gutterBottom component="h2">
+        Temperature iSpindel 2
+      </Typography>
       <Query query={GRAPH_QUERY} variables={idThree}>
         {({ data, error, loading }) => {
           if (loading) return <p>Loading...</p>;
@@ -101,7 +112,7 @@ function GraphChart() {
             // success! Prep timestamps...
             data = data.graph.graphData;
             for (var i = 0; i < data.length; i++) {
-              data[i].time = new Date(data[i].time).toLocaleString('en-GB');
+              // data[i].time = new Date(data[i].time).toLocaleString('en-GB');
             }
           }
           return (
