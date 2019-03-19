d06f8c08483d6ab25e4b146d914470cb539b4429
Peter de Lange <lange@dbis.rwth-aachen.de>
2019-03-19 23:14:45 +0100

    graph charts display limit to 100

--- components/GraphChart.js
+++ components/GraphChart.js
@@ -15,7 +15,7 @@ import Error from './Error';
 const GRAPH_QUERY = gql`
   query GRAPH_QUERY($id: ID!) {
     graph(id: $id) {
-      graphData(last: 1000, orderBy: time_ASC) {
+      graphData(last: 10000, orderBy: time_ASC) {
         time
         value
       }
@@ -28,27 +28,49 @@ function GraphChart() {
   const tempId = { id: 'cjtal4hjv005m0808e10lbyr2' };
   const idTwo = { id: 'cjteu8hdt01wk08196nvoix9c' };
   const idThree = { id: 'cjtevr4cn037808192d3kp6l2' };
-
   return (
     <div>
       <Typography variant="h4" gutterBottom component="h2">
         Temperature Fridge
       </Typography>
-      <Query query={GRAPH_QUERY} variables={tempId}>
+      <Query query={GRAPH_QUERY} variables={tempId} pollInterval={30000}>
         {({ data, error, loading }) => {
           if (loading) return <p>Loading...</p>;
           if (error) return <Error error={error} />;
           if (data && data.graph && data.graph.graphData) {
-            // success! Prep timestamps...
-            data = data.graph.graphData;
-            for (var i = 0; i < data.length; i++) {
-              // data[i].time = new Date(data[i].time).toLocaleString('en-GB');
+            // why this render data? because call is executed twice for whatever reason
+            // (caching?)...
+            var renderDataLength = 100;
+            var nthElement = Math.floor(data.graph.graphData.length / renderDataLength);
+            if (data.graph.graphData.length < renderDataLength) {
+              renderDataLength = data.graph.graphData.length;
+              nthElement = 1;
             }
+            var renderData = [];
+            for (var i = 0; i < renderDataLength - 1; i++) {
+              renderData[i] = {
+                time: new Date(data.graph.graphData[i * nthElement].time).toLocaleString(
+                  'en-GB'
+                ),
+                value: data.graph.graphData[i * nthElement].value
+              };
+            }
+            renderData[renderDataLength - 1] = {
+              time: new Date(
+                data.graph.graphData[
+                  data.graph.graphData.length - 1
+                ].time
+              ).toLocaleString('en-GB'),
+              value:
+                data.graph.graphData[
+                  data.graph.graphData.length - 1
+                ].value
+            };
           }
           return (
             // 99% per https://github.com/recharts/recharts/issues/172
             <ResponsiveContainer width="99%" height={320}>
-              <LineChart data={data}>
+              <LineChart data={renderData}>
                 <XAxis dataKey="time" />
                 <YAxis />
                 <CartesianGrid vertical={false} strokeDasharray="3 3" />
@@ -58,6 +80,7 @@ function GraphChart() {
                   type="monotone"
                   dataKey="value"
                   stroke="#8884d8"
+                  dot={false}
                   activeDot={{ r: 8 }}
                   name="Temperature"
                 />
@@ -69,30 +92,49 @@ function GraphChart() {
       <Typography variant="h4" gutterBottom component="h2">
         Tilt iSpindel 2
       </Typography>
-      <Query query={GRAPH_QUERY} variables={idTwo}>
+      <Query query={GRAPH_QUERY} variables={idTwo} pollInterval={30000}>
         {({ data, error, loading }) => {
           if (loading) return <p>Loading...</p>;
           if (error) return <Error error={error} />;
           if (data && data.graph && data.graph.graphData) {
-            // success! Prep timestamps...
-            data = data.graph.graphData;
-            for (var i = 0; i < data.length; i++) {
-              // data[i].time = new Date(data[i].time).toLocaleString('en-GB');
+            var renderDataLength = 100;
+            var nthElement = Math.floor(data.graph.graphData.length / renderDataLength);
+            if (data.graph.graphData.length < renderDataLength) {
+              renderDataLength = data.graph.graphData.length;
+              nthElement = 1;
             }
+            var renderData = [];
+            for (var i = 0; i < renderDataLength-1; i++) {
+              renderData[i] = {
+                time: new Date(data.graph.graphData[i*nthElement].time).toLocaleString(
+                  'en-GB'
+                ),
+                value: data.graph.graphData[i * nthElement].value
+              };
+            }
+            renderData[renderDataLength - 1] = {
+              time: new Date(data.graph.graphData[data.graph.graphData.length-1].time).toLocaleString(
+                'en-GB'
+              ),
+              value: data.graph.graphData[data.graph.graphData.length - 1].value};
           }
           return (
             // 99% per https://github.com/recharts/recharts/issues/172
             <ResponsiveContainer width="99%" height={320}>
-              <LineChart data={data}>
-                <XAxis dataKey="time" />
+              <LineChart data={renderData}>
+                <XAxis dataKey="time" interval={10} />
                 <YAxis />
-                <CartesianGrid vertical={false} strokeDasharray="3 3" />
+                <CartesianGrid
+                  vertical={false}
+                  strokeDasharray="3 3"
+                />
                 <Tooltip />
                 <Legend />
                 <Line
                   type="monotone"
                   dataKey="value"
                   stroke="#8884d8"
+                  dot={false}
                   activeDot={{ r: 8 }}
                   name="Tilt"
                 />
@@ -104,30 +146,55 @@ function GraphChart() {
       <Typography variant="h4" gutterBottom component="h2">
         Temperature iSpindel 2
       </Typography>
-      <Query query={GRAPH_QUERY} variables={idThree}>
+      <Query query={GRAPH_QUERY} variables={idThree} pollInterval={30000}>
         {({ data, error, loading }) => {
           if (loading) return <p>Loading...</p>;
           if (error) return <Error error={error} />;
           if (data && data.graph && data.graph.graphData) {
-            // success! Prep timestamps...
-            data = data.graph.graphData;
-            for (var i = 0; i < data.length; i++) {
-              // data[i].time = new Date(data[i].time).toLocaleString('en-GB');
+            var renderDataLength = 100;
+            var nthElement = Math.floor(data.graph.graphData.length / renderDataLength);
+            if (data.graph.graphData.length < renderDataLength) {
+              renderDataLength = data.graph.graphData.length;
+              nthElement = 1;
             }
+            var renderData = [];
+            for (var i = 0; i < renderDataLength - 1; i++) {
+              renderData[i] = {
+                time: new Date(data.graph.graphData[i * nthElement].time).toLocaleString(
+                  'en-GB'
+                ),
+                value: data.graph.graphData[i * nthElement].value
+              };
+            }
+            renderData[renderDataLength - 1] = {
+              time: new Date(
+                data.graph.graphData[
+                  data.graph.graphData.length - 1
+                ].time
+              ).toLocaleString('en-GB'),
+              value:
+                data.graph.graphData[
+                  data.graph.graphData.length - 1
+                ].value
+            };
           }
           return (
             // 99% per https://github.com/recharts/recharts/issues/172
             <ResponsiveContainer width="99%" height={320}>
-              <LineChart data={data}>
+              <LineChart data={renderData}>
                 <XAxis dataKey="time" />
                 <YAxis />
-                <CartesianGrid vertical={false} strokeDasharray="3 3" />
+                <CartesianGrid
+                  vertical={false}
+                  strokeDasharray="3 3"
+                />
                 <Tooltip />
                 <Legend />
                 <Line
                   type="monotone"
                   dataKey="value"
                   stroke="#8884d8"
+                  dot={false}
                   activeDot={{ r: 8 }}
                   name="Temperature"
                 />
