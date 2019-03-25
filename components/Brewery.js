db88951cf2ef7393a81854e7753c1aa2d319f556
Peter de Lange <lange@dbis.rwth-aachen.de>
2019-03-25 17:37:49 +0100

    adjusted brewery display

--- components/Brewery.js
+++ components/Brewery.js
@@ -1,25 +1,21 @@
 import React, { Component } from 'react';
 import PropTypes from 'prop-types';
-import Typography from '@material-ui/core/Typography';
 import { withStyles } from '@material-ui/core/styles';
-import Card from '@material-ui/core/Card';
-import CardActionArea from '@material-ui/core/CardActionArea';
-import CardActions from '@material-ui/core/CardActions';
-import CardContent from '@material-ui/core/CardContent';
-import CardMedia from '@material-ui/core/CardMedia';
-import Button from '@material-ui/core/Button';
+import Typography from '@material-ui/core/Typography';
+import Paper from '@material-ui/core/Paper';
 import Grid from '@material-ui/core/Grid';
 
-const styles = () => ({
+const styles = theme => ({
   root: {
+    textAlign: 'center',
+    paddingTop: theme.spacing.unit * 2,
     flexGrow: 1
   },
-  card: {
-    width: 400,
-    margin: 'auto'
-  },
+  container: {},
   media: {
-    height: 500
+    height: 400,
+    maxWidth: '100%',
+    objectFit: 'cover'
   }
 });
 
@@ -29,136 +25,61 @@ class Brewery extends Component {
 
     return (
       <div className={classes.root}>
-        <Grid container className={classes.root} spacing={16}>
-          <Grid item xs={12}>
-            <Grid
-              container
-              className={classes.demo}
-              justify="center"
-              spacing={8}
-            >
-              <Grid item>
-                <Card className={classes.card}>
-                  <CardActionArea>
-                    <CardMedia
-                      className={classes.media}
-                      image="../static/mashTun.png"
-                      title="The Mash Tun"
-                    />
-                    <CardContent>
-                      <Typography gutterBottom variant="h5" component="h2">
-                        Mash Tun
-                      </Typography>
-                      <Typography component="p">
-                        Here are the details...
-                      </Typography>
-                    </CardContent>
-                  </CardActionArea>
-                  <CardActions>
-                    <Button size="small" color="primary">
-                      Learn More
-                    </Button>
-                  </CardActions>
-                </Card>
-              </Grid>
-              <Grid item>
-                <Card className={classes.card}>
-                  <CardActionArea>
-                    <CardMedia
-                      className={classes.media}
-                      image="../static/grainMill.png"
-                      title="The Grain Mill"
-                    />
-                    <CardContent>
-                      <Typography gutterBottom variant="h5" component="h2">
-                        The Grain Mill
-                      </Typography>
-                      <Typography component="p">
-                        Here are the details...
-                      </Typography>
-                    </CardContent>
-                  </CardActionArea>
-                  <CardActions>
-                    <Button size="small" color="primary">
-                      Learn More
-                    </Button>
-                  </CardActions>
-                </Card>
-              </Grid>
-              <Grid item>
-                <Card className={classes.card}>
-                  <CardActionArea>
-                    <CardMedia
-                      className={classes.media}
-                      image="../static/fermenter.png"
-                      title="The Fermenter"
-                    />
-                    <CardContent>
-                      <Typography gutterBottom variant="h5" component="h2">
-                        Fermenter
-                      </Typography>
-                      <Typography component="p">
-                        Here are the details...
-                      </Typography>
-                    </CardContent>
-                  </CardActionArea>
-                  <CardActions>
-                    <Button size="small" color="primary">
-                      Learn More
-                    </Button>
-                  </CardActions>
-                </Card>
-              </Grid>
-              <Grid item>
-                <Card className={classes.card}>
-                  <CardActionArea>
-                    <CardMedia
-                      className={classes.media}
-                      image="../static/spargingVessel.png"
-                      title="Sparging Vessel"
-                    />
-                    <CardContent>
-                      <Typography gutterBottom variant="h5" component="h2">
-                        Sparging Vessel
-                      </Typography>
-                      <Typography component="p">
-                        Here are the details...
-                      </Typography>
-                    </CardContent>
-                  </CardActionArea>
-                  <CardActions>
-                    <Button size="small" color="primary">
-                      Learn More
-                    </Button>
-                  </CardActions>
-                </Card>
-              </Grid>
-              <Grid item>
-                <Card className={classes.card}>
-                  <CardActionArea>
-                    <CardMedia
-                      className={classes.media}
-                      image="../static/cooler.png"
-                      title="A Plate Cooler"
-                      height="14000"
-                    />
-                    <CardContent>
-                      <Typography gutterBottom variant="h5" component="h2">
-                        A Plate Cooler
-                      </Typography>
-                      <Typography component="p">
-                        Here are the details...
-                      </Typography>
-                    </CardContent>
-                  </CardActionArea>
-                  <CardActions>
-                    <Button size="small" color="primary">
-                      Learn More
-                    </Button>
-                  </CardActions>
-                </Card>
-              </Grid>
-            </Grid>
+        <Grid
+          container
+          className={classes.container}
+          justify="center"
+          spacing={8}
+        >
+          <Grid item>
+            <Paper>
+              <img
+                className={classes.media}
+                src="../static/breweryImages/grainMill.png"
+                title="Grain Mill"
+              />
+              <Typography>The Grain Mill</Typography>
+            </Paper>
+          </Grid>
+          <Grid item>
+            <Paper>
+              <img
+                className={classes.media}
+                src="../static/breweryImages/mashTun.png"
+                title="The Mash Tun"
+              />
+              <Typography>The Mash Tun</Typography>
+            </Paper>
+          </Grid>
+          <Grid item>
+            <Paper>
+              <img
+                className={classes.media}
+                src="../static/breweryImages/spargingVessel.png"
+                title="Sparging Vessel"
+              />
+              <Typography>The Sparging Vessel</Typography>
+            </Paper>
+          </Grid>
+          <Grid item>
+            <Paper>
+              <img
+                className={classes.media}
+                src="../static/breweryImages/cooler.png"
+                title="Wort Chiller"
+              />
+              <Typography>The Wort Chiller</Typography>
+            </Paper>
+          </Grid>
+          <Grid item>
+            <Paper>
+              <img
+                className={classes.media}
+                src="../static/breweryImages/fermenter.png"
+                title="The"
+              />
+              <Typography>The Fermenter</Typography>
+            </Paper>
           </Grid>
         </Grid>
       </div>
