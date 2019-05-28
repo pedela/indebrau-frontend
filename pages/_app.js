99f5d9747032819b750886ce463efba078e11335
PedeLa <lange@dbis.rwth-aachen.de>
2019-05-28 21:11:48 +0200

    start migration to material ui 4

--- pages/_app.js
+++ pages/_app.js
@@ -1,22 +1,18 @@
 import React from 'react';
 import App, { Container } from 'next/app';
-import { MuiThemeProvider } from '@material-ui/core/styles';
+import { ThemeProvider } from '@material-ui/styles';
 import CssBaseline from '@material-ui/core/CssBaseline';
-import JssProvider from 'react-jss/lib/JssProvider';
 import { ApolloProvider } from 'react-apollo';
-import getPageContext from '../lib/getPageContext';
 import Page from '../components/Page';
 import withData from '../lib/withData';
+import { theme } from '../lib/getPageContext';
+
 class MyApp extends App {
-  constructor() {
-    super();
-    this.pageContext = getPageContext();
-  }
 
   componentDidMount() {
     // Remove the server-side injected CSS.
     const jssStyles = document.querySelector('#jss-server-side');
-    if (jssStyles && jssStyles.parentNode) {
+    if (jssStyles) {
       jssStyles.parentNode.removeChild(jssStyles);
     }
   }
@@ -25,24 +21,16 @@ class MyApp extends App {
     const { Component, apollo, pageProps } = this.props;
     return (
       <Container>
-        <JssProvider
-          registry={this.pageContext.sheetsRegistry}
-          generateClassName={this.pageContext.generateClassName}
-        >
-          <MuiThemeProvider
-            theme={this.pageContext.theme}
-            sheetsManager={this.pageContext.sheetsManager}
-          >
-            <ApolloProvider client={apollo}>
-              <CssBaseline />
-              {/* Pass pageContext to the _document though the renderPage enhancer
-                to render collected styles on server-side. */}
-              <Page>
-                <Component pageContext={this.pageContext} {...pageProps} />
-              </Page>
-            </ApolloProvider>
-          </MuiThemeProvider>
-        </JssProvider>
+        <ThemeProvider theme={theme}>
+          <ApolloProvider client={apollo}>
+            <CssBaseline />
+            {/* Pass pageContext to the _document though the renderPage enhancer
+              to render collected styles on server-side. */}
+            <Page>
+              <Component pageContext={this.pageContext} {...pageProps} />
+            </Page>
+          </ApolloProvider>
+        </ThemeProvider>
       </Container>
     );
   }
