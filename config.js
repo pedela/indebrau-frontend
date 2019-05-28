c96e9f9c9a47a3654f91c0b0e102906e59318fc4
PedeLa <lange@dbis.rwth-aachen.de>
2019-05-28 21:22:16 +0200

    ..and further..

--- config.js
+++ config.js
@@ -1,2 +1,2 @@
-//export const endpoint = 'http://localhost:4000';
-export const endpoint = 'https://indebrau-backend.herokuapp.com';
+export const endpoint = 'http://localhost:4000';
+//export const endpoint = 'https://indebrau-backend.herokuapp.com';
