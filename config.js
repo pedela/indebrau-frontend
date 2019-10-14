9d3815e52b2ac3688ad3bcfb87fd7c7da07c55ab
Peter de Lange <lange@dbis.rwth-aachen.de>
2019-10-14 15:41:07 +0200

    .

--- config.js
+++ config.js
@@ -1,2 +1,2 @@
-export const endpoint = 'http://localhost:4000';
-//export const endpoint = 'https://api.indebrau.de';
+//export const endpoint = 'http://localhost:4000';
+export const endpoint = 'https://api.indebrau.de';
